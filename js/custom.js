bb5e2b3ec641ef536a7699f4b2fb1ea374ce15c9
evgenkarp01 <ewal@list.ru>
2018-09-05 14:14:47 +0000

    05092018

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+	$('.slick-slider-service').slick({
+		prevArrow: '.prev-services',
+		nextArrow: '.nest-services'
+	});
+
+	$('.advantages-slick').slick({
+		prevArrow: '.prev-review',
+		nextArrow: '.nest-review'
+	});
+});
